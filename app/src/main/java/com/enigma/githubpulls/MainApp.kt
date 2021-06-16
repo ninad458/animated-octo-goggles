27ed64995b28ccde92f38c5d362e9b71fb9106e4
enigma <ninadthakare458@gmail.com>
2021-06-17 02:56:25 +0530

    Hilt setup

---
+++ app/src/main/java/com/enigma/githubpulls/MainApp.kt
@@ -0,0 +1,7 @@
+package com.enigma.githubpulls
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApp : Application()
---
+++ app/src/main/java/com/enigma/githubpulls/features/main/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.enigma.githubpulls.features.main
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.enigma.githubpulls.R
+import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
