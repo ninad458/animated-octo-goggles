c61f9be58982e40475f94c173762aed35ac61fdc
enigma <ninadthakare458@gmail.com>
2021-06-18 04:28:48 +0530

    UI basic github Pulls

--- app/src/main/java/com/enigma/githubpulls/features/lists/pull/PullViewHolder.kt
+++ app/src/main/java/com/enigma/githubpulls/features/lists/pull/PullViewHolder.kt
@@ -1,12 +1,23 @@
 package com.enigma.githubpulls.features.lists.pull
 
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.enigma.githubpulls.databinding.ItemPullBinding
 import com.enigma.githubpulls.model.pull.Pull
+import java.text.SimpleDateFormat
+import java.util.*
 
 class PullViewHolder(private val binding: ItemPullBinding) : RecyclerView.ViewHolder(binding.root) {
+    private val dateFormat: SimpleDateFormat =
+        SimpleDateFormat("dd/MMM/yyyy hh:mm:s", Locale.getDefault())
 
     fun bindData(pull: Pull) {
-        binding.textView.text = pull.toString()
+        binding.apply {
+            txtTitle.text = pull.title
+            txtCreatedAt.text = dateFormat.format(pull.createdAt)
+            txtClosedAt.text = dateFormat.format(pull.closedDate)
+            txtUserName.text = pull.user.userName
+            Glide.with(itemView).load(pull.user.userIcon).into(imgUserIcon)
+        }
     }
 }
