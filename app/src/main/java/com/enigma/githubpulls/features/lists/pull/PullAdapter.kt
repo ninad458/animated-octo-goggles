3a0d53961de899708c67d21d1418fdb29b71225b
enigma <ninadthakare458@gmail.com>
2021-06-18 04:06:41 +0530

    binding in pullviewholder

--- app/src/main/java/com/enigma/githubpulls/features/lists/pull/PullAdapter.kt
+++ app/src/main/java/com/enigma/githubpulls/features/lists/pull/PullAdapter.kt
@@ -3,18 +3,17 @@ package com.enigma.githubpulls.features.lists.pull
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.enigma.githubpulls.R
+import com.enigma.githubpulls.databinding.ItemPullBinding
 import com.enigma.githubpulls.model.pull.Pull
 
 class PullAdapter : RecyclerView.Adapter<PullViewHolder>() {
 
     private var pulls: List<Pull> = emptyList()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        PullViewHolder(
-            LayoutInflater.from(parent.context)
-                .inflate(R.layout.item_pull, parent, false)
-        )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PullViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return PullViewHolder(ItemPullBinding.inflate(inflater, parent, false))
+    }
 
     override fun onBindViewHolder(holder: PullViewHolder, position: Int) {
         holder.bindData(pulls[position])
--- app/src/main/java/com/enigma/githubpulls/features/lists/pull/PullViewHolder.kt
+++ app/src/main/java/com/enigma/githubpulls/features/lists/pull/PullViewHolder.kt
@@ -1,17 +1,10 @@
 package com.enigma.githubpulls.features.lists.pull
 
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.enigma.githubpulls.R
 import com.enigma.githubpulls.databinding.ItemPullBinding
 import com.enigma.githubpulls.model.pull.Pull
 
-class PullViewHolder(parent: View) : RecyclerView.ViewHolder(
-    parent
-) {
-    private val binding = ItemPullBinding.bind(parent)
+class PullViewHolder(private val binding: ItemPullBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun bindData(pull: Pull) {
         binding.textView.text = pull.toString()
