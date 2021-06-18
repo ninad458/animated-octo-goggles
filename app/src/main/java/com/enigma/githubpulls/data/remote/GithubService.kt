77a49085e619e137cbd7630c5fca9eec7f7806e2
enigma <ninadthakare458@gmail.com>
2021-06-18 13:29:48 +0530

    remove libraries and resolve the flow

--- app/src/main/java/com/enigma/githubpulls/data/remote/GithubService.kt
+++ app/src/main/java/com/enigma/githubpulls/data/remote/GithubService.kt
@@ -3,13 +3,15 @@ package com.enigma.githubpulls.data.remote
 import com.enigma.githubpulls.data.remote.resp.PullRes
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 interface GithubService {
     @GET("repos/ninad458/animated-octo-goggles/pulls?state=closed")
-    suspend fun getPullRequests(): List<PullRes>
+    fun getPullRequests(): Call<List<PullRes>>
 
     companion object{
         fun getInstance(): GithubService {
--- app/src/main/java/com/enigma/githubpulls/model/pull/mapper/PullMapper.kt
+++ app/src/main/java/com/enigma/githubpulls/model/pull/mapper/PullMapper.kt
@@ -3,12 +3,9 @@ package com.enigma.githubpulls.model.pull.mapper
 import com.enigma.githubpulls.data.remote.resp.PullRes
 import com.enigma.githubpulls.model.pull.Pull
 import com.enigma.githubpulls.model.user.User
-import dagger.Reusable
 import java.text.SimpleDateFormat
-import javax.inject.Inject
 
-@Reusable
-class PullMapper @Inject constructor(private val simpleDateFormat: SimpleDateFormat) {
+class PullMapper(private val simpleDateFormat: SimpleDateFormat) {
 
     fun mapToUI(pullRes: PullRes): Pull? {
         return Pull(
